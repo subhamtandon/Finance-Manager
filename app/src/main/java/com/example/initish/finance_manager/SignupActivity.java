f6a5d9a11a9f1dc2fe04e6c40cfcb24c70d5d272
adityabisoi <adityabisoi1999@gmail.com>
2018-08-23 02:13:33 +0530

    signup_added

---
+++ app/src/main/java/com/example/initish/finance_manager/SignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.initish.finance_manager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
